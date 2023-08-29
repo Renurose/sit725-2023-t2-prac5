fadcbd3922d83273b9ff2136afa10b42bb0335fd
Renu Rose George <s223513383@deakin.edu.au>
2023-08-29 18:35:48 +1000

    commit

--- models/cat.js
+++ models/cat.js
@@ -9,5 +9,8 @@ function postCat(cat, callback) {
 function getAllCats(callback) {
     collection.find({}).toArray(callback);
 }
+function removeCat(cat, callback) {
+    collection.deleteOne(cat, callback)
+}
 
-module.exports = {postCat,getAllCats}
+module.exports = { postCat, getAllCats, removeCat }
--- server.js
+++ server.js
@@ -1,67 +1,14 @@
 let express = require('express');
 let app = express();
-let port = process.env.port || 3000;
-require('./dbconnection'); 
-let router = require('./routers/router'); 
-/*const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://renurose2402:user@cluster0.930i3w2.mongodb.net/?retryWrites=true&w=majority";
-let collection;*/
+let port = process.env.port || 3001;
+require('./dbconnection');
+let router = require('./routers/route')
 
 app.use(express.static(__dirname + '/'));
 app.use(express.json());
-app.use(express.urlencoded({extended: false}));
-app.use('/api/cat',router);
+app.use(express.urlencoded({ extended: false }));
+app.use('/api/cat', router);
 
-/*const client = new MongoClient(uri, {
-    serverApi: {
-      version: ServerApiVersion.v1,
-      strict: true,
-      deprecationErrors: true,
-    }
-  });
-
-async function runDB() {
-    try {
-      // Connect the client to the server	(optional starting in v4.7)
-      await client.connect();
-      collection = client.db().collection('Cats');
-      console.log(collection);
-    } catch (ex) {
-        console.error(ex);
-    }
-}
-
-function insertCat(cat,callback) {
-    collection.insertOne(cat,callback);
-}
-
-function getAllCats(callback){
-    collection.find({}).toArray(callback);
-}
-
-app.get('/', function (req,res) {
-    res.render('index.html');
-});
-
-app.post('/api/cat', (req,res)=>{
-    let cat = req.body;
-    console.log(cat);
-    insertCat(cat, (err,result) => {
-        if(!err){
-            res.json({statusCode:201,data:result,message:'success'});
-        }
-    });
-});
-
-app.get('/api/cats', (req,res)=>{
-    getAllCats((err,result)=>{
-        console.log(result);
-        if(!err){
-            res.json({statusCode:200,data:result,message:'success'});
-        }
-    });
-});*/
-
-app.listen(port, ()=>{
+app.listen(port, () => {
     console.log('express server started');
 });
